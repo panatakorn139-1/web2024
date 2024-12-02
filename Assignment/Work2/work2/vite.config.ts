0ec3df8eb0ddfb8098efe3bf3e21f09d9199134c
panatakorn139-1 <panatakorn.s@kkumail.com>
2024-12-02 16:11:05 +0700

    -

--- Assignment/Work2/work2/vite.config.ts
+++ Assignment/Work2/work2/vite.config.ts
@@ -7,7 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/web2024/',
+  base: '/web2024/Assignment/Work2/work2/dist/',
   plugins: [
     vue(),
     vueJsx(),
